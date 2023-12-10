da7a5729e82359c7eaed5160cafa6b5fd295f5d2
SeoJumee <official.jumee@gmail.com>
2023-12-10 17:05:45 +0900

    Add send page

---
+++ apps/client/src/pageContainer/SendPage/index.tsx
@@ -0,0 +1,115 @@
+'use client';
+
+import { Header, WriteButton } from 'client/components';
+import * as S from './style';
+import { useState } from 'react';
+
+const Send = () => {
+  let coins = 46;
+
+  const [coinsToSend, setCoinsToSend] = useState<number | null>(null);
+  const [coinsError, setCoinsError] = useState<boolean>(false);
+  const [selectedScope, setSelectedScope] = useState<
+    'PUBLIC' | 'PRIVATE' | null
+  >('PRIVATE');
+  const [coinsErrorMessage, setCoinsErrorMessage] = useState<string>('');
+
+  const ERROR_MESSAGES = {
+    exceedCoins:
+      '복주머니 배송비는 엽전 2닢입니다, 선물 금액은 배송비를 고려해 정해주세요!',
+    lessThanOne: '다른 사람의 엽전을 뺏어 올 수는 없어요!',
+  };
+
+  const notNullCoinToSend = coinsToSend ?? 0;
+
+  const validateCoinsToSend = (value: number) => {
+    let errorMessage = '';
+    let hasError = false;
+
+    if (value > coins - 2) {
+      errorMessage = ERROR_MESSAGES.exceedCoins;
+      hasError = true;
+    } else if (value < 0) {
+      errorMessage = ERROR_MESSAGES.lessThanOne;
+      hasError = true;
+    }
+
+    setCoinsErrorMessage(errorMessage);
+    setCoinsError(hasError);
+  };
+
+  const handleButtonClick = (scope: 'PUBLIC' | 'PRIVATE') => {
+    setSelectedScope((prev) => (prev === scope ? null : scope));
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.valueAsNumber;
+    Number.isNaN(value) ? setCoinsToSend(null) : setCoinsToSend(value);
+    validateCoinsToSend(value);
+  };
+
+  return (
+    <S.Send>
+      <S.Background>
+        <Header hasNorigae />
+        <S.ContentWrapper>
+          <S.Section>
+            <S.SectionTitleWrapper>
+              <S.Title>엽전으로 마음을 표현해봐요</S.Title>
+              <S.SectionOption>(선택)</S.SectionOption>
+            </S.SectionTitleWrapper>
+            <S.CoinInput
+              value={coinsToSend ?? ''}
+              onChange={handleInputChange}
+              placeholder='보낼 엽전 개수를 입력해 주세요.'
+              error={coinsError}
+              type='number'
+              onKeyDown={(e) =>
+                ['e', 'E', '+', '-'].includes(e.key) && e.preventDefault()
+              }
+            />
+            <S.SectionDetailWrapper>
+              <S.SectionDetail>
+                현재 보유중인 엽전 개수 <S.Coins>{coins}닢</S.Coins>
+              </S.SectionDetail>
+              {coinsError ? (
+                <S.Caption>{coinsErrorMessage}</S.Caption>
+              ) : (
+                <S.SectionDetail>{ERROR_MESSAGES.exceedCoins}</S.SectionDetail>
+              )}
+            </S.SectionDetailWrapper>
+          </S.Section>
+          <S.Section>
+            <S.SectionTitleWrapper isColumn>
+              <S.Title>복주머니를 익명으로 보낼 수 있어요</S.Title>
+              <S.SectionDetail>
+                비공개 복주머니는 엽전을 소모해 발신인을 볼 수 있어요.
+              </S.SectionDetail>
+            </S.SectionTitleWrapper>
+            <S.ScopeButtonWrapper>
+              <S.ScopeButton
+                onClick={() => handleButtonClick('PRIVATE')}
+                isClicked={selectedScope === 'PRIVATE'}
+              >
+                비공개
+              </S.ScopeButton>
+              <S.ScopeButton
+                onClick={() => handleButtonClick('PUBLIC')}
+                isClicked={selectedScope === 'PUBLIC'}
+              >
+                공개
+              </S.ScopeButton>
+            </S.ScopeButtonWrapper>
+          </S.Section>
+        </S.ContentWrapper>
+        <WriteButton
+          disabled={coinsError || selectedScope === null}
+          beforeButton='/write'
+          nextLink='/complete'
+        />
+      </S.Background>
+    </S.Send>
+  );
+};
+
+export default Send;
---
+++ apps/client/src/pageContainer/SendPage/style.tsx
@@ -0,0 +1,138 @@
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+
+export const Send = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Background = styled.div`
+  position: relative;
+  width: 38rem;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: ${({ theme }) => theme.color.ivory['010']};
+`;
+
+export const ContentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 9.288299155609169vh;
+`;
+
+export const Section = styled.div`
+  width: 33rem;
+`;
+
+export const SectionTitleWrapper = styled.div<{ isColumn?: boolean }>`
+  display: flex;
+  gap: 0.25rem;
+
+  ${({ isColumn }) =>
+    isColumn &&
+    css`
+      flex-direction: column;
+    `}
+`;
+
+export const Title = styled.h1`
+  ${({ theme }) => theme.typo.title.sub_md}
+`;
+
+export const SectionOption = styled.span`
+  ${({ theme }) => theme.typo.text.small}
+  color:${({ theme }) => theme.color.gray['040']};
+  padding-top: 0.3rem;
+`;
+
+export const CoinInput = styled.input<{ error: boolean }>`
+  ${({ theme }) => theme.typo.text.small}
+
+  width: 100%;
+  height: 3.1875rem;
+  outline: none;
+  border: 0.0625rem solid ${({ theme }) => theme.color.gray['040']};
+  border-radius: 0.625rem;
+  background: none;
+  padding-left: 1.25rem;
+  margin-top: 1.25rem;
+
+  &:focus {
+    ${({ error, theme }) =>
+      !error &&
+      css`
+        border: 0.0625rem solid ${theme.color.sub.brown['100']};
+      `}
+  }
+
+  ${({ error, theme }) =>
+    error &&
+    css`
+      border: 0.0625rem solid ${theme.color.error['050']};
+    `}
+
+  &::-webkit-outer-spin-button,
+  &::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+`;
+
+export const SectionDetailWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  margin-top: 0.5rem;
+`;
+
+export const SectionDetail = styled.span`
+  ${({ theme }) => theme.typo.text.caption}
+  color: ${({ theme }) => theme.color.gray['040']};
+`;
+
+export const Coins = styled.span`
+  ${({ theme }) => theme.typo.text.caption}
+  color: ${({ theme }) => theme.color.gray['090']};
+`;
+
+export const Caption = styled.span`
+  ${({ theme }) => theme.typo.text.caption}
+  color: ${({ theme }) => theme.color.error['050']};
+`;
+
+export const ScopeButtonWrapper = styled.div`
+  display: flex;
+  gap: 0.75rem;
+  margin-top: 1.25rem;
+`;
+
+export const ScopeButton = styled.button<{ isClicked: boolean }>`
+  ${({ theme }) => theme.typo.text.small}
+  padding: 0.6rem 1.25rem 0.5rem 1.25rem;
+  border: 0.0625rem solid ${({ theme }) => theme.color.gray['040']};
+  border-radius: 0.625rem;
+  color: ${({ theme }) => theme.color.gray['040']};
+
+  &:not(:disabled):hover {
+    ${({ isClicked, theme }) =>
+      !isClicked &&
+      css`
+        border: 0.0625rem solid ${theme.color.sub.brown['500']};
+        color: ${theme.color.sub.brown['100']};
+        background-color: #eee3d6;
+      `}
+  }
+
+  ${({ isClicked, theme }) =>
+    isClicked &&
+    css`
+      border: 0.0625rem solid ${theme.color.sub.brown['100']};
+      color: ${theme.color.ivory['010']};
+      background-color: ${theme.color.sub.brown['100']};
+    `}
+`;
