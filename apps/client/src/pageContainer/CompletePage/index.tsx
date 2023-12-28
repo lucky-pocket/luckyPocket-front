7e08f57284224215a389b06263eea3087d83f9ff
SeoJumee <official.jumee@gmail.com>
2023-12-28 19:51:43 +0900

    Add Complete page setting

---
+++ apps/client/src/pageContainer/CompletePage/index.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import { Header } from 'client/components';
+import * as S from './style';
+
+export default function Complete() {
+  return (
+    <S.Complete>
+      <S.Background>
+        <Header hasNorigae />
+      </S.Background>
+    </S.Complete>
+  );
+}
---
+++ apps/client/src/pageContainer/CompletePage/style.ts
@@ -0,0 +1,19 @@
+import styled from '@emotion/styled';
+
+export const Complete = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Background = styled.div`
+  position: relative;
+  width: 38rem;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: ${({ theme }) => theme.color.ivory['010']};
+`;
