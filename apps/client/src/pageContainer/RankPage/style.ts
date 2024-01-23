8792c31e2347ae2ae97ba83b89e9e8787a2a0766
예빈 <yebin060310@gmail.com>
2024-01-23 15:45:50 +0900

    Update px -> rem

--- apps/client/src/pageContainer/RankPage/style.ts
+++ apps/client/src/pageContainer/RankPage/style.ts
@@ -2,7 +2,6 @@ import styled from '@emotion/styled';
 
 export const Rank = styled.div`
   width: 100vw;
-  height: 100vh;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -11,7 +10,6 @@ export const Rank = styled.div`
 export const Background = styled.div`
   position: relative;
   width: 38rem;
-  height: 100vh;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -24,7 +22,11 @@ export const Conatiner = styled.div`
   flex-direction: column;
   justify-content: center;
   gap: 1.875rem;
-  height: 100%;
+  height: 100vh;
   overflow: hidden;
-  margin-top: 5.9375rem;
+  position: relative;
+  top: 4.375rem;
+  @media (max-height: 62.75rem) {
+    top: 6.25rem;
+  }
 `;
