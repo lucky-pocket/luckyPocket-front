c66019eb993c2d9af57154470e96279ebc68394d
예빈 <yebin060310@gmail.com>
2024-02-07 22:06:05 +0900

    Update lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,73 +16,18 @@ module.exports = {
   },
   plugins: ['@typescript-eslint', 'react'],
   rules: {
-    rules: {
-      'no-unused-vars': 'off',
-      '@typescript-eslint/no-unused-vars': ['error'],
-      'sort-imports': [
-        'error',
-        {
-          ignoreCase: true,
-          ignoreDeclarationSort: true,
-          ignoreMemberSort: false,
-          allowSeparatedGroups: true,
-        },
-      ],
-      'import/order': [
-        'error',
-        {
-          'newlines-between': 'always',
-          groups: ['builtin', ['external', 'type'], 'internal'],
+    'arrow-body-style': ['error', 'as-needed'],
+    'react/self-closing-comp': ['error', { component: true, html: true }],
+    '@typescript-eslint/consistent-type-imports': [
+      'error',
+      {
+        prefer: 'type-imports',
+      },
+    ],
+    'no-console': 'error',
 
-          pathGroups: [
-            {
-              pattern: 'next',
-              group: 'external',
-              position: 'before',
-            },
-            {
-              pattern: 'next/**',
-              group: 'external',
-              position: 'before',
-            },
-            {
-              pattern: 'react',
-              group: 'external',
-              position: 'before',
-            },
-            {
-              pattern: 'components',
-              group: 'internal',
-              position: 'after',
-            },
-            {
-              pattern: 'shared/**',
-              group: 'internal',
-              position: 'after',
-            },
-            {
-              pattern: 'utils/**',
-              group: 'internal',
-              position: 'after',
-            },
-            {
-              pattern: 'types/**',
-              group: 'internal',
-              position: 'after',
-            },
-            {
-              pattern: 'styles/**',
-              group: 'internal',
-              position: 'after',
-            },
-          ],
-
-          alphabetize: {
-            order: 'asc',
-            caseInsensitive: true,
-          },
-        },
-      ],
-    },
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error'],
+    use: 2,
   },
 };
