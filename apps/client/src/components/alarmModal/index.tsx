712eccaaa3303864845300cdd94d9b2c0cb7399d
SeoJumee <official.jumee@gmail.com>
2024-02-09 00:30:17 +0900

    Add get notice

--- apps/client/src/components/alarmModal/index.tsx
+++ apps/client/src/components/alarmModal/index.tsx
@@ -1,45 +1,84 @@
 import { Coin, EyesIcon, LuckyPocket } from 'client/assets';
 import * as S from './style';
+import { useQuery } from '@tanstack/react-query';
+import { API } from 'api/client/API';
+import { userMyNoticeUrl, userMyUrl } from 'api/client';
 
 interface propsType {
   modalRef: React.ForwardedRef<HTMLDivElement>;
   modalOutSideClick: (e: any) => void;
 }
 
+interface noticeType {
+  id: string;
+  kind: string;
+  pocketId: number;
+  checked: boolean;
+  createdAt: string;
+}
+
 const AlarmModal = ({ modalRef, modalOutSideClick }: propsType) => {
-  const data = [
-    {
-      svg: <LuckyPocket />,
-      title: '복주머니가 왔어요!',
-      time: '30분 전',
-    },
-    {
-      svg: <EyesIcon />,
-      title: '누군가 복주머니를 열람했어요!',
-      time: '30분 전',
-    },
-  ];
+  const getNotice = async () => {
+    const response = await API.get(userMyNoticeUrl.getNotice());
+    return response.data;
+  };
+
+  const { data } = useQuery(['getNotice'], () => getNotice());
+
+  const getMyCoins = async () => {
+    const response = await API.get(userMyUrl.getMyCoin());
+    return response.data;
+  };
+
+  const { data: coinsData } = useQuery<{ coins: number }>(['getMyCoin'], () =>
+    getMyCoins()
+  );
+
+  const detailDate = (a: any) => {
+    const milliSeconds = (new Date() as any) - a;
+    const seconds = milliSeconds / 1000;
+    if (seconds < 60) return `방금 전`;
+    const minutes = seconds / 60;
+    if (minutes < 60) return `${Math.floor(minutes)}분 전`;
+    const hours = minutes / 60;
+    if (hours < 24) return `${Math.floor(hours)}시간 전`;
+    const days = hours / 24;
+    if (days < 7) return `${Math.floor(days)}일 전`;
+    const weeks = days / 7;
+    if (weeks < 5) return `${Math.floor(weeks)}주 전`;
+    const months = days / 30;
+    if (months < 12) return `${Math.floor(months)}개월 전`;
+    const years = days / 365;
+    return `${Math.floor(years)}년 전`;
+  };
 
   return (
-    <S.Conatiner ref={modalRef} onClick={(e) => modalOutSideClick(e)}>
+    <S.Container ref={modalRef} onClick={(e) => modalOutSideClick(e)}>
       <S.Wrapper>
         <S.CoinWrapper>
           <Coin />
-          <S.CoinCount>123</S.CoinCount>
+          <S.CoinCount>{coinsData?.coins}</S.CoinCount>
         </S.CoinWrapper>
         <S.AlarmWrapper>
-          {data.map((data, idx) => (
-            <S.AlarmItem key={idx}>
-              <S.AlarmTitle>
-                {data.svg}
-                <S.AlarmContent>{data.title}</S.AlarmContent>
-              </S.AlarmTitle>
-              <S.AlarmTime>{data.time}</S.AlarmTime>
-            </S.AlarmItem>
-          ))}
+          {data &&
+            data.notices.map((data: noticeType) => (
+              <S.AlarmItem key={data.id}>
+                <S.AlarmTitle>
+                  {data.kind === 'REVEALED' ? <EyesIcon /> : <LuckyPocket />}
+                  <S.AlarmContent isRead={data.checked}>
+                    {data.kind === 'REVEALED'
+                      ? '누군가 복주머니를 열람했어요!'
+                      : '복주머니가 왔어요!'}
+                  </S.AlarmContent>
+                </S.AlarmTitle>
+                <S.AlarmTime>
+                  {detailDate(new Date(data.createdAt))}
+                </S.AlarmTime>
+              </S.AlarmItem>
+            ))}
         </S.AlarmWrapper>
       </S.Wrapper>
-    </S.Conatiner>
+    </S.Container>
   );
 };
 
