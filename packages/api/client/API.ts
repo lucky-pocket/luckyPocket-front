a1012b5207b4f00f1cc06c0a9f04dc3328cf138f
SeoJumee <official.jumee@gmail.com>
2024-02-10 23:58:01 +0900

    Update refresh

--- packages/api/client/API.ts
+++ packages/api/client/API.ts
@@ -22,7 +22,7 @@ API.interceptors.request.use(async (config: InternalAxiosRequestConfig) => {
   if (!accessToken || !expiresAt) return config;
 
   if (
-    new Date() > new Date('Sat Feb 11 2024 23:50:45 GMT+0900 (한국 표준시)')
+    new Date() > new Date('Sun Feb 11 2024 00:03:00 GMT+0900 (한국 표준시)')
   ) {
     axios
       .post(process.env.NEXT_PUBLIC_CLIENT_API_URL + authUrl.postRefresh())
