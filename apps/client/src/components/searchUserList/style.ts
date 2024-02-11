a6e440a3f5de433b58840e8a31981655d213ad18
SeoJumee <official.jumee@gmail.com>
2024-02-12 00:24:42 +0900

    Fix search list style

--- apps/client/src/components/searchUserList/style.ts
+++ apps/client/src/components/searchUserList/style.ts
@@ -7,6 +7,7 @@ export const UserListWrapper = styled.div`
   flex-direction: column;
   gap: 1rem;
   overflow: scroll;
+  padding-bottom: 3.75rem;
   &::-webkit-scrollbar {
     display: none;
   }
@@ -14,7 +15,7 @@ export const UserListWrapper = styled.div`
 
 export const BackgroundFilter = styled.div`
   width: 100%;
-  height: 5.625rem;
+  height: 3.8rem;
   position: absolute;
   bottom: 0;
   background: linear-gradient(0, #f2ede5 3.22%, rgba(242, 237, 229, 0) 94.49%);
